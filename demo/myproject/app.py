aec21ea18b979230cdc03d3adf7052d2e13fdf32
김지수 <040310jisoo@naver.com>
2023-03-06 18:40:11 +0900

    2023.3.6
    1. Error처리 
    2. 텍스트 마이닝

--- demo/myproject/app.py
+++ demo/myproject/app.py
@@ -1,98 +1,138 @@
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 import pymysql
+import time
 from flask_sqlalchemy import SQLAlchemy
 from konlpy.tag import Okt
 import numpy as np  # 이미지 데이터를 다루기 위해
 import pandas as pd  # 데이터를 다루기 위한 라이브러리
 from wordcloud import WordCloud
 from PIL import Image  # 이미지를 위한 라이브러리
-import time
+import os
+from collections import Counter
 import matplotlib.pyplot as plt
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
+
+
+#from sklearn.feature_extraction.text import CountVectorizer
 db = SQLAlchemy()
 app = Flask(__name__)
 
 # rest api부분 =====================================
 
-
+# /  : html template 보여줌
 @app.route('/')
 def index():
+    #os.remove("./static/images/wordcloud_img.png")
     return render_template('index.html')
 
 
-# @app.route('/text',  methods=["POST"])
-def test():
-   # data = request.get_json()
-   # print(data)
-    return "asdf"
 
+# 크롤링 데이터를 insert하는 db 부분 ============================
+def insertDB(k, t, c1, c2, r):
+    ret = []
+    db = pymysql.connect(host='localhost', user='root',
+                            port=3307,password='1234', charset='utf8', db='mydb')
+    curs = db.cursor()
+    sqlInsert = """
+        insert into mydb.CrawlingNaverArticles(KeyWord, Title, ChangeContents, Contents, Registration_Date ) values( '{KeyWord1}' ,  '{Title1}', '{Contents1}', '{Contents2}',  '{Registration_Date1}' );
+        """.format(KeyWord1=k, Title1=t, Contents1=c1, Contents2=c2, Registration_Date1=r )
+    curs.execute(sqlInsert)
+    db.commit()
+    #db.close()
+    return ret
 
-class MyEmpDao:
-    def getEmps(self):
-        ret = []
-        db = pymysql.connect(host='localhost', user='root',
-                             port=3307, db='mydb', password='1234', charset='utf8')
-        curs = db.cursor()
-
-        sql = "select * from mydb.ServerDB"
-        curs.execute(sql)
-
-        rows = curs.fetchall()
-        for e in rows:
-            ret.append(e)
-        db.commit()
-        db.close()
-        return ret
-
-
-# 크롤링한 text data를 매개변수에 전달해서 =====================
+# 워드 클라우드 부분 ============================
 
 def word(text):
-    
-    f = open("./static/dd.txt", "w", encoding='utf-8') # 파일 생성 
-    print(text)
-        # 크롤링한 데이터값 변수에 저장
-    
-    okt = Okt()
-    nouns = okt.nouns(text) 
-
-    # 명사만 추출
-    for i in range(len(nouns)):
-        f.write(nouns[i])
-
-
-    dd = ' '.join(v for v in nouns)
-
-    wordcloud = WordCloud(font_path="./static/malgun.ttf",max_words=20).generate(dd)
+   # print("======== 워드클라우드 시작 dd2=======")
+    wordcloud = WordCloud(font_path="./static/malgun.ttf",max_words=100, width = 800 , height = 400 ,background_color='white').generate(text)
     
     plt.figure()
     plt.axis('off')
     plt.imshow(wordcloud, interpolation='bilinear')
-    plt.show()
-# 크롤리 다 하고 디비에 넣고 디비에서 select핵서 화면에 뿌려주기
-# 그럼 메모리 공간을 어떻게 해야하지?
-# 디비에서 키워드에 맞는 문서 찾아다가 문서안에서 단어 쪼개서 wordcloud로 보여주기
+    os.remove('./static/images/wordcloud_img.png')
+    
+    plt.savefig('./static/images/wordcloud_img.png', bbox_inches='tight')
 
-#table마다 다르게 만들기 키워드 별로 만들면 될듯 그럼 ㅇㅋ
+   
+
+# 크롤링 데이터를 select db 부분 ============================
+
+
 
 @app.route('/text',  methods=["POST"])
+def selectDB():
+    kyeword = request.get_json()        
+    key = kyeword['keyword']
+    print(key)
+    ret = []
+    db = pymysql.connect(host='localhost', user='root',
+                            port=3307,password='1234', charset='utf8', db='mydb')
+    curs = db.cursor()
+
+   
+    sqlInsert =  """ 
+            SELECT ChangeContents
+            FROM  mydb.CrawlingNaverArticles
+            WHERE Contents LIKE '%{keyword1}%';
+            """.format(keyword1=key)
+    
+
+    curs.execute(sqlInsert)
+  
+
+    for i in curs:
+        s = ''.join(map(str, i))
+        ret.append(s)
+    
+    text = ''.join(list(set(ret)))
+    last_arr = []
+    arr = text.split(" ") 
+    
+    for i in arr:
+        if i == "통해" or i == "지난해"  or i == "예상" or i == "사진"  or i == "오늘" or i == "최근" or i == "지난" or i == "위해"or i == "때문" or i == "대한" or i == "대해" or i == "이후" or i == "관련" or i == "라며" or i == "이번" or i == "또한" or i == "거나" :
+            continue
+        else:
+            s = ''.join(map(str, i))
+            last_arr.append(s)
+            
+    
+    text1 = ' '.join(last_arr)
+    
+    word(text1) 
+    strings = text1.split()
+    
+    test_list2 = []
+    social_news_word_count = Counter(strings)
+    for i in social_news_word_count.most_common(50):
+        test_list2.append(i)
+
+
+
+
+    db.commit()
+    db.close()
+    return test_list2
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9988)
+
+
+"""
+# 크롤링하는 부분 ============================
+@app.route('/text',  methods=["POST"]) #  html 에서 받은 키워드 python으로 받기 위함
 def crolling():
     kyeword = request.get_json()
-    print(kyeword['keyword'])
     key = kyeword['keyword']
-
     dr = webdriver.Chrome(executable_path='C:/Windows/chromedriver.exe')
-    for page_num in range(0, 1):
-        print("dr ======")
-        # time.sleep(3)
+    for page_num in range(0, 10):
         # range를 이용하면 0부터 인덱스가 시작되므로 page_num에 1을 더해준 url을 이용
         url = f'https://search.naver.com/search.naver?where=news&sm=tab_pge&query={key}&sort=0&photo=0&field=0&pd=0&ds=&de=&cluster_rank=93&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so:r,p:all,a:all&start={page_num}1'
-        print(dr)
+        
         ActionChains(dr)  # 드라이버에 동작을 실행시키는 명령어를 act로 지정
         dr.get(url)
         html = dr.page_source
@@ -108,59 +148,82 @@ def crolling():
             if link.get('href').find('https://n.news.naver.com/mnews/article') != -1: 
                 dr.get(link['href'])
                 time.sleep(2)
-                # 사이트 들어갔는데 url주소가 -> https://n.news.naver.com/mnews/article
-                # 이게 들어가는 사이트이다? 그럼 contents 텍스트를 가져오고 
-                # 아니라면 페이지를 전단계로 뒤로 돌린다.
-                
                 if dr.current_url.find('https://n.news.naver.com/mnews/article')  != -1:
-                    contents = dr.find_element(By.ID, 'dic_area')
-                    text =  contents.text
-                    print(text)
+                    
+                    kyeword = request.get_json()
+                    key = kyeword['keyword'] # 키워드 
+                    contents = dr.find_element(By.ID, 'dic_area') # 네이버 기사 컨텐츠 글
+                    title = dr.find_element(By.ID, 'title_area') # 네이버 기사 제목 
+                    date_time = dr.find_element(By.CLASS_NAME, '_ARTICLE_DATE_TIME') # 네이버 기사 입력 날짜  
+               
+                    contents_text = '\"'.join(contents.text.split("'"))
+                    title_text = '\"'.join(title.text.split("'"))
+                    date_time_text = '\"'.join(date_time.text.split("'"))
+                    
+  
+                    print("================ title_text ===============\n")
+                  
+                    print(title_text)
+                    print( "기사 입력 날짜 : " , date_time_text, "키워드 : " , key)
+                    print("================ contents_text ===============\n")
+                    print(contents_text)
+                    # word(text) # word 클라우드 보여주는 함수에 파라미터 전달
                     
                     time.sleep(2)
-                    print("\n===================================")
+
+                     #형태소 분석 
+                
+                    okt = Okt()
+                    nouns = okt.nouns(contents_text) 
+
+
+                    dd = ' '.join(v for v in nouns)
+                    # print("======== 워드클라우드 시작 dd =======")
+                    # print(dd)
+                    list = dd.split(' ')
+                            #print("======== 워드클라우드 시작 =======")
+                    test_list = []
+                    for item in list:
+                        if  len(item) > 1:
+                            test_list.append(item)
+                    
+
+                        
+                    dd2 = ' '.join(v for v in test_list)
+                    list2 = dd2.split(' ')
+
+                    test_list2 = []
+                    social_news_word_count = Counter(list2)
+                    for i in social_news_word_count.most_common():
+                        if i[0] == '기자':
+                            continue
+                        elif  i[0] == '오전':
+                            continue
+                        elif  i[0] == '때문':
+                            continue
+                        elif  i[0] == '오후':
+                            continue
+                        else :
+                            test_list2.append(i[0])
+                            #print(i)
+
+                        
+                    contents_text_change = ' '.join(v for v in test_list2)
+                    list3 = contents_text_change.split(' ')
+
+
+
+                    insertDB(key, title_text, contents_text_change, contents_text , date_time_text)
+                    print("\n\n\n\n================================== 다음 글 ==================================")
                     dr.back()
                 else :
                     dr.back()
                     
-                #
                 #time.sleep(5)
-        print(page_num) 
+        #print(page_num) 
           
     return kyeword
 
 
 """
-파일 내용 삭제 
-    try:
-    with open("./static/hitsong.txt", "w",'r+', encoding='utf-8') as f:
-        f.truncate() # 파일 생성 
-except IOError:
-    print('Failure')
 
-"""
-
-if __name__ == '__main__':
-   # emplist = MyEmpDao().getEmps()
-   # print(emplist)
-    app.run(debug=True, host='0.0.0.0', port=9090)
-
-
-
-"""
-def wordclouding(textdata):
-    #굳이 파일로 변환안하고 형속변환만 해서 바로 돌려줘도 될듯 
-    df = pd.read_csv('./static/test.txt', delimiter = '\t')
-    text = ''.join(v for v in df['대한민국헌법'])
-    okt = Okt()
-    nouns = okt.morphs(text) # 명사만 추출
-    dd = ' '.join(v for v in nouns)
-
-    wordcloud = WordCloud( font_path = "./static/malgun.ttf",max_words=10).generate(dd)
-
-    plt.figure()
-    plt.axis('off')
-    plt.imshow(wordcloud, interpolation='bilinear') 
-    plt.show()
-    #cv2.imwrite('./static/images', img)
-"""
--- demo/myproject/static/common.js
+++ demo/myproject/static/common.js
@@ -1,25 +1,70 @@
 $(document).ready(function () {
+   for(let i =0; i< 50; i++){
+        $('#tb').append('<tr><td class="nth_number"></td><td class="data"></td></tr>');
+   }
+    for(let i = 0; i < $('.nth_number').length; i++){
+        $('.nth_number')[i].innerText = i + "위"
+    }
+    
     $('#inputBox2').click(function () {
         var keyword =  $("#inputText").val()
         var postdata = {
             'keyword':keyword
         }
-        console.log(JSON.stringify(postdata))
-      // var textDsata = "국민의 권리와 의무  모든 국민은 인간으로서의 존엄과 가치"
-        $.ajax({
-            type: 'POST',
-            url: '/text',
-            data: JSON.stringify(postdata),
-            dataType : 'JSON',
-            contentType: "application/json",
-            success: function (data) {
-                alert(data) //연결을 확인한다
-                //console.log( data)
-            },
-            error: function(){
-                alert('ajax 통신 실패')
-            }
-        })
-
+        let asdf  = []
+        
+        // 유효성 검사 
+        if($("#inputText").val() == ""){
+            alert("키워드를 입력해주세요")
+            $("#inputText").focus()
+        }else{
+            $.ajax({
+                type: 'POST',
+                url: '/text',
+                data: JSON.stringify(postdata),
+                dataType : 'JSON',
+                contentType: "application/json",
+                success : function(data) {
+                    // 입력한 키워드의 빈도수 순위별 데이터 나타내기
+                    for(let i = 0; i <data.length; i++){
+                        let a= data[i].join(' / ')
+                        $('.data')[i].innerText = a
+                        asdf.push(a)
+                    }
+                    localStorage.setItem("key", asdf);
+                    console.log($("#go").val()  == "보기")
+                    setInterval(function(){
+                        $("#go").css("background-color", "#00ef01")
+                        if ($("#go").val() == "보기") {
+                            $("#go").val(" ")
+                        } else {
+                            $("#go").val("보기")
+                        }   
+                    }, 500) 
+                },
+                error:function(error){
+                    alert("찾을 수 없는 키워드 입니다. 다시 입력해주세요."); 
+                    $("#inputText").val(" ")
+                    $("#inputText").focus()// 실패 시 처리
+                }
+                   
+            })
+            
+            // 클릭시 페이지 리로드함
+            $('#go').on({'click': function(){
+                console.log("click함")
+                window.location.reload()
+                }
+            });
+        }
     });
+  
+    // 전 데이터 새로고침 후에도 유지하기 ( 데이터 테이블  & 이미지 )
+    let output = localStorage.getItem("key");		
+    let s = output.split(",")
+    for(let i = 0; i <s.length; i++){
+        $('.data')[i].innerText = s[i]
+    }
+    $("#wordcloud_img").attr("src", "../static/images/wordcloud_img.png");    
+    $("#wordcloud_img").width('100%');
 })
